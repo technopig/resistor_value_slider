379efa296b07c6141440fcf365ba82e5551f4fb0
Sam Cipriani <sjcip@umich.edu>
2017-10-21 00:37:12 -0400

    fixed up code and readme

--- src/resistor_value_slider.ino
+++ src/resistor_value_slider.ino
@@ -7,27 +7,63 @@
 char auth[] = "5487a1b9f1f34f2ab1d5872ec64e35f6";
 int resist_aVal = 0;
 int blynk_sVal = 0;
+int onoff = 0;
+
+BlynkTimer timer;
+
+BLYNK_WRITE(V1){
+  blynk_sVal = param.asInt();
+}
+
+BLYNK_WRITE(V2){
+  onoff = param.asInt();
+}
+
+void sendResistVal()
+{
+  Blynk.virtualWrite(V0, resist_aVal);
+}
 
 void setup()
 {
-  // Debug console
-
   delay(500); // Allow board to settle
-  //Blynk.begin(auth);
-  pinMode(A4, INPUT); // Resistor input
-  pinMode(A5, INPUT); // Blynk slider input
-  pinMode(A0, OUTPUT); //output steady voltage
+
+  Particle.variable("resistVal", &resist_aVal, INT); //register real resistor measured value
+  Particle.variable("blynkVal", &blynk_sVal, INT); //register blynk slider Value
+
+  Blynk.begin(auth); //begin Blynk
+  timer.setInterval(1000L, sendResistVal); //every 1000ms, execute sendResistVal(){}
+
+  pinMode(A4, INPUT); // Analog resistor input
+
+  pinMode(D7, OUTPUT); //control LED with Blynk
+
+  //Output steady voltage on A0
+  pinMode(A0, OUTPUT);
   digitalWrite(A0, HIGH);
-  Particle.variable("analogvalue", &resist_aVal, INT);
-  Particle.variable("blynkVal", &blynk_sVal, INT);
 }
 
 void loop()
 {
-  resist_aVal = analogRead(A4);
-  blynk_sVal = analogRead(A5);
+  Blynk.run();
+  timer.run();
+  resist_aVal = analogRead(A4); //regular analogread on A4
+
+  Blynk.virtualWrite(V3, onoff*255); //write to the virtual LED
+
+  if (onoff)
+  {
+      digitalWrite(D7, HIGH);
+
+  }
+  else
+  {
+      digitalWrite(D7, LOW);
+  }
+
   delay(200);
 }
+
 
 
 
